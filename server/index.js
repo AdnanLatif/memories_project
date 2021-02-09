3ed7f2a72c6290da9ffb250e6a0b5e3922b3628d
Adnan Latif <alatif.bwp@gmail.com>
2021-02-09 20:27:32 +0500

    Controllers and models are added for server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoute from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoute);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limmit: '30mb0', extended: true }));
