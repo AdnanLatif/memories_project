f3ba80cab9957a69b87b8d0b032eb9c83fdb2687
Adnan Latif <alatif.bwp@gmail.com>
2021-02-12 19:37:54 +0500

    Some formatting and .env file added

--- server/index.js
+++ server/index.js
@@ -2,22 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoute from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limmit: '30mb0', extended: true }));
 app.use(cors());
 app.use('/posts', postRoute);
 
-const CONNECTION_URL =
-  'mongodb+srv://adnan:igeu98MRkP3iMB6@cluster0.nzhjq.mongodb.net/memoriesProject?retryWrites=true&w=majority';
+('mongodb+srv://adnan:igeu98MRkP3iMB6@cluster0.nzhjq.mongodb.net/memoriesProject?retryWrites=true&w=majority');
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
