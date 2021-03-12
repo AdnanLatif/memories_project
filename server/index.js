701fd8c9411eaf609c05bc7ef6542bc027084c92
Adnan Latif <alatif.bwp@gmail.com>
2021-03-12 17:25:36 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
 const CONNECTION_URL =
-  'mongoDB URI';
+  'mongodb+srv://adnan:igeu98MRkP3iMB6@cluster0.nzhjq.mongodb.net/memoriesProject?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
