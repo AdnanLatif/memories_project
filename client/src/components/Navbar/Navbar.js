860de0a15312554005b0cd86266dd5e59a7c7a98
Adnan Latif <alatif.bwp@gmail.com>
2021-03-12 15:14:24 +0500

    Handled Token expiry.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
@@ -21,9 +22,14 @@ const Navbar = (props) => {
   };
 
   useEffect(() => {
-    const auth = user?.token;
+    const token = user?.token;
+    if (token) {
+      const decodedToken = decode(token);
 
-    //JWT
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
