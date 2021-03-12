83c5017a3194060f6b78c0eaef2df3c9fa09cde7
Adnan Latif <alatif.bwp@gmail.com>
2021-03-12 14:52:55 +0500

    Wrap edit button in auth.
    - User can edit or delete it's own post.

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -66,15 +66,18 @@ const Post = ({ post, setCurrentId }) => {
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button
-          style={{ color: 'white' }}
-          size='small'
-          onClick={() => setCurrentId(post._id)}
-        >
-          <MoreHorizIcon fontSize='default' />
-        </Button>
-      </div>
+      {(user?.result?.googleId === post?.creator ||
+        user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2}>
+          <Button
+            onClick={() => setCurrentId(post._id)}
+            style={{ color: 'white' }}
+            size='small'
+          >
+            <MoreHorizIcon fontSize='default' />
+          </Button>
+        </div>
+      )}
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary' component='h2'>
           {post.tags.map((tag) => `#${tag} `)}
